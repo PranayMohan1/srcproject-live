15a23e7e946cb821559a81951d68104277634574
PranayMohan1 <mohanpranay22@gmail.com>
2020-08-05 21:24:31 +0530

    c4

--- myapi/settings.py
+++ myapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q82qb^$er)63jmt0v*ghkwdd=0l#ir@k91@dmmtiocpg3s!7$&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boiling-refuge-59072.herokuapp.com','127.0.0.1']
 
 
 # Application definition
